7ab40c66a66f466e1c9855b382dccbcff91e9d2d
rlatjsdn818 <sunwoo030404@g.shingu.ac.kr>
2025-11-12 15:29:56 +0900

    스크립트 먼저 올려둠

---
+++ 2022137039_game_25_2/Assets/Scripts/1112/SearchBackTracking.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchBackTracking : MonoBehaviour
+{
+    int[] cards = { 2, 3, 5, 7, 9 };
+
+    int limit = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Search(0, new List<int>(), 0);
+    }
+    void Search(int i, List<int> list, int sum)
+    {
+        Debug.Log("========================");
+        Debug.Log($"Search Called : i = {i}");
+
+        if (sum > limit) return;
+        if (i == cards.Length)
+        {
+            Debug.Log($"{string.Join(",", list)} = {sum}");
+            return;
+        }
+        //현재 카드 선택
+        list.Add(cards[i]);
+        Search(i + 1, list, sum + cards[i]);
+        list.RemoveAt(list.Count - 1);
+
+        //현재 카드 미선택
+        Search(i + 1, list, sum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
